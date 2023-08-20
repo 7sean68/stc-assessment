776e43fbd56da9ffb7a97c42306e08f915c6c47c
Hussein Aassem <hussienasem@gmail.com>
2023-08-20 06:51:12 +0300

    fixed native query

--- src/main/java/com/hussien/stcassessment/repositories/ItemRepository.java
+++ src/main/java/com/hussien/stcassessment/repositories/ItemRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.hussien.stcassessment.domain.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    @Query(value = "SELECT * FROM public.file where id = :id", nativeQuery = true)
+    @Query(value = "SELECT * FROM public.item where id = :id", nativeQuery = true)
     Optional<Item> findByIdNative(Long id);
 }
