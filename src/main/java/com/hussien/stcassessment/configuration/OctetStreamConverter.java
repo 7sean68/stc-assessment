bfbdcb8aaedfa7eecfe7d5394821fdebd53a5b13
Hussein Aassem <hussienasem@gmail.com>
2023-08-20 06:50:34 +0300

    fixed multipart

---
+++ src/main/java/com/hussien/stcassessment/configuration/OctetStreamConverter.java
@@ -0,0 +1,18 @@
+package com.hussien.stcassessment.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class OctetStreamConverter {
+
+    public OctetStreamConverter(MappingJackson2HttpMessageConverter converter) {
+        List<MediaType> supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
+}
